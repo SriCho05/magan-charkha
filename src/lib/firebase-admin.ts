6b106b7f7eff399b990f98dc434b2ae9d2a657cc
sri cho <chosri2005@gmail.com>
2025-07-27 08:06:50 +0000

    do i need to implement something like this for admin

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,23 +1,36 @@
 
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import 'dotenv/config'
 
 const apps = getApps();
 
-const BOGO_APP_NAME = 'bogo-admin-app';
+const ADMIN_APP_NAME = 'khadi-kraft-admin';
 
 let adminApp: App;
 
-if (!apps.find((app) => app.name === BOGO_APP_NAME)) {
-  adminApp = initializeApp(
-    {
-      // Using App Default Credentials, so no need to specify credentials here
-      // projectId is also inferred from the environment
-    },
-    BOGO_APP_NAME
-  );
+if (!apps.find((app) => app.name === ADMIN_APP_NAME)) {
+  const serviceAccountString = process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS;
+
+  if (!serviceAccountString) {
+    throw new Error('The GOOGLE_SERVICE_ACCOUNT_CREDENTIALS environment variable is not set. Please generate a service account key in the Firebase console and set it in your .env file.');
+  }
+  
+  try {
+    const serviceAccount = JSON.parse(serviceAccountString);
+    adminApp = initializeApp(
+      {
+        credential: cert(serviceAccount),
+      },
+      ADMIN_APP_NAME
+    );
+  } catch(e) {
+    console.error("Could not parse service account credentials. Make sure it's a valid JSON object in your .env file.")
+    throw e;
+  }
+
 } else {
-  adminApp = apps.find((app) => app.name === BOGO_APP_NAME)!;
+  adminApp = apps.find((app) => app.name === ADMIN_APP_NAME)!;
 }
 
 export const adminDb = getFirestore(adminApp);
